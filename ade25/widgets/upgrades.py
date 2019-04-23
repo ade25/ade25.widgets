246b3c08299e954def1135910a7fd5e13f7c14f7
Christoph Böhner-Figas <boehner@team23.de>
2019-04-23 16:18:11 +0200

    Add assets folder

--- ade25/widgets/upgrades.py
+++ ade25/widgets/upgrades.py
@@ -29,3 +29,15 @@ def upgrade_1001(setup):
     setup.runImportStepFromProfile(default_profile, 'plone.app.registry')
     # Update registry settings
     update_widget_settings()
+
+
+def upgrade_1002(setup):
+    setup.runImportStepFromProfile(default_profile, 'typeinfo')
+    portal = api.portal.get()
+    # Create a folder for widget asset management if needed
+    if 'asset-repository' not in portal:
+        api.content.create(
+            container=portal,
+            type='ade25.widgets.assetsfolder',
+            id='asset-repository',
+            title=u'Asset Repository')
