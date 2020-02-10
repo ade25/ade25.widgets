820f653f4edf8cc47879bd259fd7979a20aa7657
Christoph Böhner-Figas <boehner@team23.de>
2020-02-10 17:02:59 +0100

    [FEATURE] Content widget base content (wip)

--- ade25/widgets/widgets/interfaces.py
+++ ade25/widgets/widgets/interfaces.py
@@ -1,12 +1,12 @@
 # -*- coding: utf-8 -*-
 """Module providing widget settings"""
-# -*- coding: utf-8 -*-
-"""Module providing standalone content panel edit forms"""
+from plone.app.textfield import RichText
 from plone.autoform import directives as form
 from plone.autoform.interfaces import IFormFieldProvider
 from Products.CMFPlone.utils import safe_unicode
 from zope import schema
 from zope.interface import Interface, provider
+from plone.app.z3cform.widget import LinkFieldWidget
 
 from ade25.widgets import MessageFactory as _
 
@@ -32,4 +32,33 @@ class IAde25WidgetPartialReadMore(Interface):
         required=False,
         default='link',
         vocabulary='ade25.widgets.vocabularies.ContentWidgetReadMeLayoutOptions'
+    )
+
+
+@provider(IFormFieldProvider)
+class IAde25WidgetPartialContentMain(Interface):
+    """ Content widget main elements """
+
+    headline = schema.TextLine(
+        title=_(u"Headline"),
+        required=False
+    )
+
+    abstract = schema.Text(
+        title=_(u"Abstract"),
+        required=False
+    )
+
+    text = RichText(
+        title=_(u"Text"),
+        required=False
+    )
+
+    form.widget(link=LinkFieldWidget)
+    link = schema.TextLine(
+        title=_(u"Link"),
+        description=_(u"Optional internal or external link that will be "
+                      u"used as redirection target when section is accessed."
+                      u"Logged in users will see the target link instead."),
+        required=False,
     )
